91d7f53a7d87c2d035530207e4f7e1c99130143d
Noa Resare <noa@resare.com>
2026-03-25 14:08:52 +0000

    Remove obsolete Bytes wrapper

--- src/util.rs
+++ src/util.rs
@@ -1,11 +1,11 @@
-use crate::bytes::Bytes;
 use crate::error::IdentityError;
 use base64::engine::general_purpose::URL_SAFE_NO_PAD;
 use base64::Engine;
+use bytes::Bytes;
 use rand::{random, rng, RngCore};
 use serde::{Deserialize, Serialize};
 use std::ops::Deref;
-use surrealdb::types::{Kind, SurrealValue, Value};
+use surrealdb::types::SurrealValue;
 
 // According to the spec(1) this needs to be at least 16 bytes. We go with 18
 // as we will base64 encode the value and lengths not evenly divisible by 3 will
@@ -17,7 +17,7 @@ pub fn make_token() -> Vec<u8> {
     random::<[u8; CHALLENGE_BYTE_COUNT]>().into()
 }
 
-#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug, Clone)]
+#[derive(Serialize, Deserialize, PartialEq, PartialOrd, Debug, Clone, SurrealValue)]
 pub struct Token(Bytes);
 
 impl Token {
@@ -35,21 +35,6 @@ impl Token {
         Ok(Token(
             URL_SAFE_NO_PAD.decode(base64.as_ref().as_bytes())?.into(),
         ))
-    }
-}
-
-impl SurrealValue for Token {
-    fn kind_of() -> Kind {
-        String::kind_of()
-    }
-
-    fn into_value(self) -> Value {
-        self.base64().into_value()
-    }
-
-    fn from_value(value: Value) -> Result<Self, surrealdb::Error> {
-        let base64 = String::from_value(value)?;
-        Token::from_base64(&base64).map_err(|err| surrealdb::Error::internal(err.to_string()))
     }
 }
 
