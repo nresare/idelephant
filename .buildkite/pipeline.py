1cc29cbf4777ef3aec243d1cde57688d5c8e136e
Noa Resare <noa@resare.com>
2026-04-12 13:43:13 +0100

    Formatting, have main branch publish depend on tests

--- .buildkite/pipeline.py
+++ .buildkite/pipeline.py
@@ -9,23 +9,32 @@ import os
 from typing import Any
 from image_version import version
 
-def pipeline(tag: str, should_publish: bool=False) -> dict[str, Any]:
+
+def pipeline(tag: str, should_publish: bool = False) -> dict[str, Any]:
     rust_test = {
         "label": ":rust: rust build and test",
         "commands": [
             "cargo fmt --check",
             "cargo clippy --workspace --locked",
             "cargo test --workspace --locked",
-        ]
+        ],
     }
-    docker = {
-                "label": ":whale: build docker image",
-                "command": f"docker buildx build -t idelephant:{tag} .",
-                "agents": {"arch": "arm64"},
-            }
+    docker: dict[str, Any] = {
+        "label": ":whale: build docker image",
+        "command": f"docker buildx build -t idelephant:{tag} .",
+        "agents": {"arch": "arm64"},
+    }
     if should_publish:
+        docker["depends_on"] = "rust_test"
         docker["plugins"] = [
-            {"docker-image-push#v1.1.0": {"provider": "buildkite", "image": "idelephant", "tag": tag,"buildkite": {"auth-method": "oidc"}}},
+            {
+                "docker-image-push#v1.1.0": {
+                    "provider": "buildkite",
+                    "image": "idelephant",
+                    "tag": tag,
+                    "buildkite": {"auth-method": "oidc"},
+                }
+            },
         ]
     return {"steps": [rust_test, docker]}
 
